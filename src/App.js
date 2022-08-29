b628cbe8b85660aeb0027bf8ac5c6ff5791a7005
yasmin <yasminmartinsdebrito@gmail.com>
2022-08-28 21:49:11 -0300

    feat:criando pastar e arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+   <header>
+    <h1>Projeto Funcionando</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Fotos</li>
+        <li>Cadastrar</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+   </header>
+   <main>
+    <section>
+      <article>
+        <h2>Imagens</h2>
+      </article>
+      <p>Essa é a Pagina principal do site</p>
+      <hr></hr>
+    </section>
+
+   </main>
+   <footer>
+    <h4>Yasmin Martins de Brito</h4>
+   </footer>
+    </>
+  );
+}
+
+export default App;
